3e0b2a9a9c2e58ce06496d4f1149b0eb58067a26
Martin Podmanicky <mpodmanicky34@gmail.com>
2025-03-31 14:45:04 +0200

    Created branch martin

---
+++ stuface/components/TaskBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, StyleSheet } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+export default function TaskBar() {
+  return (
+
+  )
+}
+
+const styles = StyleSheet.create({
+    icons: {
+        padding: 10,
+        margin: 10,
+        color: "black"
+    },
+})
