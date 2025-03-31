bc62033b9c567fefe575afa4a165edeb36f99351
Martin Podmanicky <mpodmanicky34@gmail.com>
2025-03-31 23:52:30 +0200

    Adding endpoints, slowly!

--- backend/server.js
+++ backend/server.js
@@ -35,19 +35,10 @@ app.use(express.json());
  * On successfull auth sent 200, along with json user data
  */
 app.post('/register', async (req, res) => {
-  //user object typically replacable with req.body creating object alike
-  // const user = {
-  //   username: 'Username',
-  //   name: 'John',
-  //   lastname: 'Doe',
-  //   email: 'johndoe@gmail.com',
-  //   password: 'johndoe123',
-  //   password2: 'johndoe123'
-  // }
   const user = req.body;
 
   try {
-    const existing_user = await pool.query("SELECT * FROM users WHERE email = $1", [user.email]);
+    const existing_user = await pool.query("SELECT * FROM users WHERE email = $1 and username = $2", [user.email, user.username]);
     if (existing_user.rows.length === 0) {
       if (user.password === user.password2) {
         await pool.query('INSERT INTO users (username, name, lastname, email, password) VALUES ($1, $2, $3, $4, $5);', [user.username, user.name, user.lastname, user.email, user.password]);
@@ -56,7 +47,7 @@ app.post('/register', async (req, res) => {
         return res.status(400).json({ error: 'Passwords do not match' });
       }
     } else {
-      return res.status(400).json({ error: 'User with this email already exists' });
+      return res.status(409).json({ error: 'User with this email or username already exists' });
     }
   } catch (error) {
     console.error("Something went wrong when registering...", error);
@@ -99,8 +90,20 @@ app.post('/profile', async (req, res) => {});
  * User clicks on desired topic
  * Topic loads along with user posts and content
  */
-app.get('/topic', async (req, res) => {
+app.get('/topic/:id', async (req, res) => {
+  const topicId = req.params.id;
 
+  try {
+    const topicData = await pool.query("") // potrebujeme vybrat vsetky data, cize posty, k postom komentare, ich hodnotenie atd atd atd...
+    if(topicData.rows.length === 0) {
+      return res.status(200).json({ message: "Be the first to post!" });
+    } else {
+      return res.status(200).json({ data: topicData }); // posleme tonu udajov musime tam porobit spravne joiny aby to vsetko sedelo lebo s nami bude amen
+    }
+  } catch (e) {
+    console.error("Something went wrong when loading topic...", e);
+    return res.status(500).json({ error: "Internal server error!" });
+  }
 });
 
 /**
@@ -109,7 +112,22 @@ app.get('/topic', async (req, res) => {
  * He can invite people to his topic
  * 
  */
-app.post('/topic', async (req, res) => {});
+app.post('/topic', async (req, res) => {
+  const topicData = req.body;
+
+  try {
+    const userAlreadyHasTopic = await pool.query("Select * from topics where name = $1 and owner_id = $2", [topicData.topicName, topicData.user_id]);
+    if(userAlreadyHasTopic.rows.length === 0) {
+      await pool.query("INSERT INTO topics (name, owner_id, private) VALUES ($1, $2, $3)", [topicData.topicName, topicData.user_id, topicData.isPrivate]);
+      return res.status(200).json({ message: `Topic ${topicData.topicName} successfully created` });
+    } else {
+      return res.status(409).json({ error: "User already has topic with the name created!" });
+      } 
+  } catch (e) {
+    console.error("Something went wrong when adding topic...", e);
+    return res.status(500).json({ error: "Internal server error!" });
+  }
+});
 
 // simple get request to retrieve user with id 1 from the database, get usually works with uri not body
 app.get('/user', async (req, res) => {
